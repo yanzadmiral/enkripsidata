432133262473a828c5e963a4d1be86f81a4367b4
Yayan Cahyani <yayanchy@gmail.com>
2016-03-27 01:52:08 +0700

    nambah kindredagency/Encryption + yg lain2

--- enkripsidata/MainWindow.xaml.cs
+++ enkripsidata/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Framework.AssetLibrary.Encryption; 
+using Framework.AssetLibrary.Encryption.Cryptography; 
+
 
 namespace enkripsidata
 {
@@ -23,6 +26,36 @@ namespace enkripsidata
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void btndeskrip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txthasil.Text = "adsd";
+            string text = "Wee Willie Winkie runs through the town";  
+            Console.WriteLine(text); 
+
+ 
+            string textHash = text.ComputeHash(AlgorithmType.SHA512); 
+ 
+ 
+            Console.WriteLine(textHash); 
+ 
+ 
+            Console.WriteLine(text.VerifyHash(textHash, AlgorithmType.SHA512));
+
+            MessageBox.Show("" + textHash +"dan"+ text.VerifyHash(textHash, AlgorithmType.SHA512));
+
+        }
+
+        private void txt2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
